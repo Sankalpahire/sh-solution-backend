64927fef86fc1e6a09ed3831e92416f7d1970799
Sankalp <Sankalpahire2001@gmail.com>
2024-07-14 19:41:49 +0530

    port issue fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const subscriberRoutes = require('./routes/subscriberRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
